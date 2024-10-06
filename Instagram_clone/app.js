dfd8b7a46a69558be84c8249840519a4c9fe4a3f
serhatsahin28 <95092280+serhatsahin28@users.noreply.github.com>
2024-10-06 17:19:13 +0300

    responsive Updated
    home.ejs updated

--- Instagram_clone/app.js
+++ Instagram_clone/app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
 const http = require('http');
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 const session = require("express-session");
 const path = require('path');
 const bodyParser = require("body-parser");
