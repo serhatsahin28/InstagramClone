7fec85cd5d9f12d650594d607a3ffea653817244
serhatsahin28 <95092280+serhatsahin28@users.noreply.github.com>
2024-10-06 19:15:54 +0300

    sa
    sa

--- Instagram_clone/app.js
+++ Instagram_clone/app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const http = require('http');
-const PORT = 3000;
+const dotenv = require('dotenv');
+dotenv.config();
+const PORT = process.env.PORT || 3000;
 const session = require("express-session");
 const path = require('path');
 const bodyParser = require("body-parser");
