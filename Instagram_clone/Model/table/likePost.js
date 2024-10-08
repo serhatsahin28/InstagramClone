e604e56da31688a7e0764e8120b24dc35922bf2c
serhatsahin28 <95092280+serhatsahin28@users.noreply.github.com>
2024-10-08 16:56:17 +0300

    a
    a

--- Instagram_clone/Model/table/likePost.js
+++ Instagram_clone/Model/table/likePost.js
@@ -1,6 +1,8 @@
+const dotenv = require('dotenv');
+dotenv.config();
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://REDACTED:REDACTED@REDACTED/instagram").then(() => {
+mongoose.connect(process.env.MONGODB_URI).then(() => {
     // console.log("MongoDB'ye bağlanıldı");
 }).catch((err) => {
     console.error("MongoDB'ye bağlanırken hata oluştu:", err);
