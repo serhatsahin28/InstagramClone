dae99bb67de8f76413edb6e9e71ecdc42de6745d
serhatsahin28 <95092280+serhatsahin28@users.noreply.github.com>
2024-10-08 16:42:56 +0300

    make it better

--- .history/Model/table/dbMessages_20241008163719.js
+++ .history/Model/table/dbMessages_20241008163719.js
@@ -1,12 +1,11 @@
-const dotenv = require('dotenv');
-dotenv.config();
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI).then(() => {
+mongoose.connect("mongodb+srv://REDACTED:REDACTED@REDACTED/instagram").then(() => {
     // console.log("MongoDB'ye bağlanıldı");
 }).catch((err) => {
     console.error("MongoDB'ye bağlanırken hata oluştu:", err);
 });
+
 const Schema = mongoose.Schema;
 
 const message = new Schema({
